b2e9f710170dc65e8ff39f161a31ac622f74f99e
danjkim21 <dan.jkim21@gmail.com>
2022-04-02 08:47:34 -0400

    added solutions for April 2 and updated new rank in readme

--- 2022-04-02.js
+++ 2022-04-02.js
@@ -1,6 +1,20 @@
-// Get the mean of an array
-
+// 8 kyu - Get the mean of an array
 function getAverage(marks){
     //TODO : calculate the downward rounded average of the marks array
     return Math.floor(marks.reduce((acc, c) => acc + c / marks.length, 0));
   }
+
+// 8 kyu - Reversed Words
+function reverseWords(str){
+  return str.split(" ").reverse().join(" ");
+}
+
+// 8 kyu - MakeUpperCase
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+// 8 kyu - is he gonna survive?
+function hero(bullets, dragons){
+    return dragons * 2 > bullets ? false: true;
+}
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # daily-codewars-challenges
 
-This repository is where I collect my daily coding challenge solutions from Codewars.com.
+This repository is where I collect my daily coding challenge solutions from Codewars.com. <br>
 I am trying my best to get daily repetition in to keep core concepts fresh in my head, hopefully doing this out in public will help keep me accountable!
 
 ## Current Stats:
-Ranked - 7 kyu <br>
+Ranked - 6 kyu <br>
 Total Completed Kata - 39
