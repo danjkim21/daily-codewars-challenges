c377ce51dffc2a1e9c4bbe3d843b29c0f3e6b011
danjkim21 <dan.jkim21@gmail.com>
2022-07-22 08:44:37 -0400

    create and added 07-22 solutions

---
+++ 2022-07-22.js
@@ -0,0 +1,34 @@
+// Detect Pangram
+function isPangram(string) {
+  let arr = string.toLowerCase().split('');
+  letters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+
+  return letters.every((elem) => arr.includes(elem));
+}
